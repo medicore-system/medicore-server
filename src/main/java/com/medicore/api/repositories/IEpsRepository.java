31d1682e01945811923a40f3c282290cbdde2a54
manuelnorenacUam20 <manuel.norenac@autonoma.edu.co>
2026-05-12 00:30:22 -0500

    feat: EPS and appointment type logic was added to the Medicore server

--- src/main/java/com/medicore/api/repositories/IEpsRepository.java
+++ src/main/java/com/medicore/api/repositories/IEpsRepository.java
@@ -4,7 +4,6 @@ import com.medicore.api.entities.Eps;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 /**
  * Repositorio encargado de la persistencia y consulta
  * de entidades {@link Eps}.
@@ -16,12 +15,4 @@ import java.util.Optional;
  */
 @Repository
 public interface IEpsRepository extends JpaRepository<Eps, String> {
-    /**
-     * Busca una EPS por su nombre.
-     *
-     * @param nombre nombre de la EPS.
-     * @return un Optional con la EPS encontrada,
-     * o vacío si no existe.
-     */
-    Optional<Eps> findByNombre(String nombre);
 }
